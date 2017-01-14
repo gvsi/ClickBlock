f604ab99f4b1d0f7f8718cc10f5a66dd27e6793e
Martin Georgiev <martingeorgiev1995@gmail.com>
2017-01-14 12:57:06 -0800

    Add learning code

---
+++ learning/training.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import re
+from nltk.corpus import stopwords
+
+def article_to_words( article ):
+    letters_only = re.sub("[^a-zA-Z0-9]", " ", article)
+    words = letters_only.lower().split()
+    stops = set(stopwords.words("english"))
+    meaningful_words = [w for w in words if not w in stops]
+    return( " ".join( meaningful_words ))
+
+
+train = pd.read_json("training-data.json")
+
+num_titles = train["title"].size
+clean_train_titles = []
+
+
+for i in xrange( 0, num_titles ):
+    clean_train_titles.append( article_to_words( train["title"][i] ) )
+
+from sklearn.feature_extraction.text import CountVectorizer #TfidfVectorizer
+
+
+vectorizer = CountVectorizer(analyzer = "word",   \
+                             tokenizer = None,    \
+                             preprocessor = None, \
+                             stop_words = None,   \
+                             max_features = 5000)
+
+train_data_features = vectorizer.fit_transform(clean_train_titles)
+train_data_features = train_data_features.toarray()
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegressionCV
+
+forest = RandomForestClassifier(n_estimators = 100)
+forest = forest.fit( train_data_features, train["clickbait"] )
+
+#forest = LogisticRegressionCV()
+#forest = forest.fit( train_data_features, train["clickbait"] )
+
+from sklearn.externals import joblib
+joblib.dump(forest, 'learned.pkl')
+joblib.dump(vectorizer.vocabulary_, 'vectorizer.pkl')
+
+
+# TESTING
