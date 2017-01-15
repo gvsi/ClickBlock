cbf97bd06aa23f6fa0c784d52f4d6b0bfe925639
Ahan Mukhopadhyay <Ahan@dhcp-v069-045.mobile.uci.edu>
2017-01-14 20:59:53 -0800

    Upgrade

--- server/scrape_ny.py
+++ server/scrape_ny.py
@@ -7,3 +7,31 @@ from guess_language import guessLanguage
 import csv
 from bs4 import BeautifulSoup
 
+titles = []
+
+for i in range(1, 102):
+
+    # url = "http://query.nytimes.com/search/sitesearch/#/*/30days/document_type%3A%22article%22/"+ str(i) + "/allauthors/newest/"
+    url = "http://query.nytimes.com/svc/add/v1/sitesearch.json?begin_date=30daysago&sort=desc&page="+str(i)+"&fq=document_type%3A%22article%22&facet=true"
+    req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) 
+
+    # print "Reading " + url + " -------"
+    try:
+        response = urllib2.urlopen(req)
+        # print response
+        obj = json.loads(response.read())
+        for i in range(len(obj['reponse']['docs']))
+            print obj['response']['docs'][i]['headline']['main']
+            titles.append(obj)
+            print "------"
+            doc = {'clickbait': 1, 'title': obj['response']['docs'][i]['headline']['main']}
+
+
+    
+    except URLError as e:
+        print "ERROR --------"
+        print e.reason
+
+
+print "FINISHED"
+
