dec683594b8d783adcee0f791dc495ff84c1ca72
Ahan Mukhopadhyay <Ahan@dhcp-v069-045.mobile.uci.edu>
2017-01-15 12:09:35 -0800

    Corrected NY Article Reptition Flaw

--- server/scrape_ny.py
+++ server/scrape_ny.py
@@ -10,10 +10,10 @@ from bs4 import BeautifulSoup
 titles = []
 documents = []
 total = 0
-for i in range(1, 365):
+for i in range(1, 100):
 
     # url = "http://query.nytimes.com/search/sitesearch/#/*/30days/document_type%3A%22article%22/"+ str(i) + "/allauthors/newest/"
-    url = "http://query.nytimes.com/svc/add/v1/sitesearch.json?begin_date="+str(i)+"daysago&sort=desc&page=99&fq=document_type%3A%22article%22&facet=true"
+    url = "http://query.nytimes.com/svc/add/v1/sitesearch.json?begin_date="+str(365)+"daysago&sort=desc&page="+str(i)+"&fq=document_type%3A%22article%22&facet=true"
     req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) 
 
     # print "Reading " + url + " -------"
@@ -21,11 +21,11 @@ for i in range(1, 365):
         response = urllib2.urlopen(req)
         # print response
         obj = json.loads(response.read())
-        for i in range(len(obj['response']['docs'])):
-            print obj['response']['docs'][i]['headline']['main']
+        for j in range(len(obj['response']['docs'])):
+            print obj['response']['docs'][j]['headline']['main']
             titles.append(obj)
             print "------"
-            doc = {'clickbait': 0, 'title': obj['response']['docs'][i]['headline']['main']}
+            doc = {'clickbait': 0, 'title': obj['response']['docs'][j]['headline']['main']}
             documents.append(doc)
             total += 1
 
@@ -33,7 +33,6 @@ for i in range(1, 365):
     except URLError as e:
         print "ERROR --------"
         print e.reason
-        break
 
 # open file for dumping
 with open('data/new_nytimes2.json', 'w') as fp:
