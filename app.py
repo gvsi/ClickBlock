039fe5b85c3aef3a11cbdfbaa334b9c8bf6ea877
Ahan Mukhopadhyay <Ahan@dhcp-v069-045.mobile.uci.edu>
2017-01-14 18:34:49 -0800

    Adjusted app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, request, url_for
+from flask_jsonpify import jsonify
 import requests
-from flask.ext.jsonpify import jsonify
-import os
 import json
 app = Flask(__name__)
-
-# from OpenSSL import SSL
-# context = SSL.Context(SSL.SSLv23_METHOD)
-# context.use_privatekey_file(os.getcwd()+'/server/server.key')
-# context.use_certificate_file(os.getcwd()+'/server/server.crt')
 
 @app.route('/')
 def hello_world():
@@ -16,41 +10,41 @@ def hello_world():
 
 @app.route('/verifybait', methods=['POST', 'GET'])
 def verify():
-    if request.method == 'POST' or 'GET':
-        #print request.form.get('title')
-        print request.args.get('link')
+    link = request.args.get('link')
+    # title = request.args.get('title')
 
-        # do computation with title and link
-        result = "not bait"
+    print link
+    # do computation with title and link
+    # clickbait = 0 or 1
 
-        # send a post request to extention
-        # r = requests.post('http://extentionhere.com', data = {'result': result})
-        url = "https://explaintome.herokuapp.com/api/v1.0/summary"
-        headers = {'Content-Type': "application/json", 'Accept': 'application/json'}
-        data = {
-            "url": request.form.get('link'),
-            "max_sent": 1,
-            "lang": "en"
-        }
-        
-        r = requests.post(url, headers=headers, data=json.dumps(data))
-        print r.status_code
-        d = r.json()
-        
-        newtitle = d["meta"]["opengraph"]["description"].encode('ascii', 'ignore')
-        summary = d["summary"][0].encode('ascii', 'ignore')
-        
-        print "Title:", newtitle.encode('ascii', 'ignore')
-        print "Summary:", summary.encode('ascii', 'ignore')
+    # send a post request to extention
+#    r = requests.post('http://extentionhere.com', data = {'result': result})
+    url = "https://explaintome.herokuapp.com/api/v1.0/summary"
+    headers = {'Content-Type': "application/json", 'Accept': 'application/json'}
+    data = {
+        "url": link,
+        "max_sent": 1,
+        "lang": "en"
+    }
 
-        result = {'title': newtitle, 'summary': summary, 'clickbait': 1}
+    print '-----------'
+    r = requests.post(url, headers=headers, data=json.dumps(data))
+    print r.status_code
+    print '----------'
+    print r.json()
+    d = json.loads(r.text)
 
-        return jsonify(**result)
+    newtitle = d["meta"]["opengraph"]["description"].encode('ascii', 'ignore')
+    summary = d["summary"][0].encode('ascii', 'ignore')
 
-    if request.method == 'GET':
-        return 'Please make a POST request to get summary, corrected title and clickbait'
+    print "Title:", newtitle
+    print "Summary:", summary
+
+    print '\n'
+    print d
+
+    result = {'title': newtitle, 'summary':summary, 'clickbait': 1}
+    return jsonify(**result)
 
 if __name__ == '__main__':
-    # context = (os.getcwd()+'/../server.crt', os.getcwd()+'/../server.key')
-    # app.run(host='0.0.0.0', port=5000, ssl_context=context, threaded=True, debug=True)
-        app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
